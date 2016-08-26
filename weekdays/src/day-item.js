d04c780b18f99ed6051b517e52a778dcc5b36fa3
Tevin Mantock <tmantock@gmail.com>
2016-08-25 17:07:04 -0700

    dynamic dates and styling

--- weekdays/src/day-item.js
+++ weekdays/src/day-item.js
@@ -1,18 +1,37 @@
-import React, { Text, StyleSheet } from 'react-native';
+import React, { Text } from 'react-native';
 
 const DayItems = React.createClass({
   render: function() {
-    return <Text style={styles.day}>
+    return <Text style={this.style()}>
       {this.props.day}
     </Text>
+  },
+  style: function () {
+    return {
+      color: this.color(),
+      fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  },
+  color: function () {
+    if(this.props.daysUntil == 0){
+      var opacity = 1
+    } else{
+      var opacity = 1 / this.props.daysUntil;
+    }
+    return `rgba(0,0,0, ${opacity})`;
+  },
+  fontWeight: function () {
+    let weight = (7 - this.props.daysUntil) * 100;
+    return `${weight}`;
+  },
+  fontSize: function () {
+    return 60 - 6 * this.props.daysUntil;
+  },
+  lineHeight: function() {
+    return 70 - 4 * this.props.daysUntil;
   }
 });
-
-const styles = StyleSheet.create({
-  day: {
-    fontSize: 18,
-    color: '#0000FF'
-  }
-})
 
 module.exports = DayItems;
