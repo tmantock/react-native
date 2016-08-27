4c23e2e792f9160c435d7c31aaa0530da4450958
Tevin Mantock <tmantock@gmail.com>
2016-08-26 21:12:55 -0700

    Touachable added

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, AppRegistry, StyleSheet } from 'react-native';
+import { Text, View, AppRegistry, StyleSheet, TouchableHighlight } from 'react-native';
 
 const StopWatch = React.createClass({
   render: function() {
@@ -29,11 +29,11 @@ const StopWatch = React.createClass({
 },
 startStopButton: function () {
   return (
-    <View>
+    <TouchableHighlight underlayColor="gray">
       <Text>
         Start
       </Text>
-    </View>
+    </TouchableHighlight>
   );
 },
 lapButton: function () {
